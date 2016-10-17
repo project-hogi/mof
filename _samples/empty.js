aeaf279d15c98980d8c533b27e2c5386d240dfc8
조용준 <junishop@gmail.com>
2016-10-17 16:08:10 +0900

    first

---
+++ _samples/empty.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(function($) {
+
+	$(document).on("appinitOnce", function() {
+	});
+
+	$(document).on("appinit", function() {
+	});
+
+})(jQuery);
---
+++ www/views/part/header.php
@@ -0,0 +1,65 @@
+<!-- Header.html -->
+<div id="wrap">
+	<div id="header">
+		<div class="container">
+			<h1 class="logo"><a href="#"><span class="sr-only">해양수산부 통계시스템</span></a></h1>
+			<div class="lnb">
+				<ul class="depth1">
+					<li>
+						<a href="#"></a>
+						<ul class="depth2">
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#"></a>
+						<ul class="depth2">
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#"></a>
+						<ul class="depth2">
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#"></a>
+						<ul class="depth2">
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#"></a>
+						<ul class="depth2">
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+	<div id="container">
+		<div class="container">
+<!-- Header.html -->
